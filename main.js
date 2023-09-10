dd589cdfefc0b23269b6946815c24b73d28ea10c
Wiris Wernek <wiris.wernek@itixti.com.br>
2023-09-10 14:05:24 -0300

    remove pasta do bootstrap e remove inicializadores do for

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function GenerateDiv() {
     let force = document.getElementsByName("force");
 
     let forceChange = 0;
-    for (var i = 0 in force) {
+    for (var i in force) {
         if (force[i].checked) {
             forceChange = force[i].value;
         }
@@ -30,7 +30,7 @@ function Generate() {
     let force = document.getElementsByName("force");
     let forceChange = 0;
     let alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', '<', '>', '?', '~', '`', '/'];
-    for (var i = 0 in force) {
+    for (var i in force) {
         if (force[i].checked) {
             forceChange = force[i].value;
         }
